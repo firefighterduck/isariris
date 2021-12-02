cd9a08a28bb16d7405c2af2402eb66fb7d4df7ef
Florian Sextl <sextl@in.tum.de>
2021-12-02 18:27:37 +0100

    switch from locale to class and typedef and add uprop

--- IrisCore/CoreStructures.thy
+++ IrisCore/CoreStructures.thy
@@ -6,12 +6,19 @@ section \<open> Core Structures \<close>
 text \<open> This section introduces the concepts of cameras, resource algebras and related structures. \<close>
     
 (* Non-expansive functions, i.e. equivalence persisting functions from one OFE to another. *)
-locale non_expansive =
-  fixes ne :: "'a::ofe \<Rightarrow> 'b::ofe"
-  assumes ne_def: "(x,y) \<in> n_equiv n \<Longrightarrow> (ne x, ne y) \<in> n_equiv n"
+typedef (overloaded) ('a::ofe,'b::ofe) non_expansive = 
+  "{ne::'a\<Rightarrow>'b. \<forall>(x::'a) y n. (x,y) \<in> n_equiv n \<longrightarrow> (ne x, ne y) \<in> n_equiv n }"
+proof
+  fix b :: 'b
+  define ne where "ne = (\<lambda>_::'a. b)"
+  hence "\<forall>x y n. (x, y) \<in> n_equiv n \<longrightarrow> (ne x, ne y) \<in> n_equiv n" by (simp add: ofe_refl)
+  then show "ne \<in> {ne. \<forall>x y n. (x, y) \<in> n_equiv n \<longrightarrow> (ne x, ne y) \<in> n_equiv n}" by blast
+qed
+
+setup_lifting type_definition_non_expansive
 
 (* Resource algebra *)
-locale ra =
+class ra =
   fixes valid :: "'a \<Rightarrow> bool"
     and core :: "'a \<Rightarrow> 'a option"
     and comp :: "'a \<Rightarrow> 'a \<Rightarrow> 'a"
@@ -24,32 +31,94 @@ locale ra =
     and ra_valid_op: "valid (comp a b) \<Longrightarrow> valid a"
 
 (* Camera, a more general notion of resource algebras based on an OFE. *)
-locale camera =
-  ofe_m: ofe eq_m + valid_raw: non_expansive valid_raw + core: non_expansive core
-    + comp: non_expansive comp
-  for eq_m :: "nat \<Rightarrow> ('a::ofe\<times>'a) set" and valid_raw :: "'a \<Rightarrow> sprop" and core :: "'a \<Rightarrow> 'a option" 
-    and comp :: "('a\<times>'a) \<Rightarrow> 'a" +
-  assumes camera_assoc: "comp (comp (a,b),c) = comp (a,comp (b,c))"
-    and camera_comm: "comp (a,b) = comp (b,a)"
-    and camera_core_id: "\<exists>a'. core a = Some a' \<Longrightarrow> comp (the (core a),a) = a"
-    and camera_core_idem: "\<exists>a'. core a = Some a' \<Longrightarrow> core (the (core a)) = core a"
-    and camera_core_mono: "\<lbrakk>\<exists>a'. core a = Some a'; \<exists>c. b=comp (a,c)\<rbrakk> \<Longrightarrow> 
-      (\<exists>b'. (core b = Some b') \<and> (\<exists>c. (the (core b)) = comp (the (core a),c)))"
-    and camera_valid_op: "valid_raw (comp (a,b)) \<le> valid_raw a"
-    and camera_extend: "\<lbrakk>n \<le> valid_raw a; (a,comp (b1,b2)) \<in> n_equiv n\<rbrakk> \<Longrightarrow>
-      \<exists>c1 c2. (a=comp (c1,c2) \<and> (c1,b2) \<in> n_equiv n \<and> (c2,b2) \<in> n_equiv n)"
-    
-definition (in camera) valid :: "'a \<Rightarrow> bool" where
-  "valid a = (\<forall>n. n \<le> valid_raw a)"
+class camera = ofe +
+  fixes valid_raw :: "(('a::ofe),sprop) non_expansive" and core :: "('a,'a option) non_expansive" 
+    and comp :: "(('a\<times>'a),'a) non_expansive"
+  assumes camera_assoc: 
+      "Rep_non_expansive comp (Rep_non_expansive comp (a,b),c) 
+      = Rep_non_expansive comp (a,Rep_non_expansive comp (b,c))"
+    and camera_comm: 
+      "Rep_non_expansive comp (a,b) = Rep_non_expansive comp (b,a)"
+    and camera_core_id: 
+      "\<exists>a'. Rep_non_expansive core a = Some a' 
+      \<Longrightarrow> Rep_non_expansive comp (the (Rep_non_expansive core a),a) = a"
+    and camera_core_idem: 
+      "\<exists>a'. Rep_non_expansive core a = Some a' 
+      \<Longrightarrow> Rep_non_expansive core (the (Rep_non_expansive core a)) = Rep_non_expansive core a"
+    and camera_core_mono: 
+      "\<lbrakk>\<exists>a'. Rep_non_expansive core a = Some a'; \<exists>c. b=Rep_non_expansive comp (a,c)\<rbrakk> \<Longrightarrow> 
+      (\<exists>b'. (Rep_non_expansive core b = Some b') 
+      \<and> (\<exists>c. (the (Rep_non_expansive core b)) = Rep_non_expansive comp (the (Rep_non_expansive core a),c)))"
+    and camera_valid_op: 
+      "Rep_sprop (Rep_non_expansive valid_raw (Rep_non_expansive comp (a,b))) n 
+      \<Longrightarrow> Rep_sprop (Rep_non_expansive valid_raw a) n"
+    and camera_extend: 
+      "\<lbrakk>Rep_sprop (Rep_non_expansive valid_raw a) n; (a,Rep_non_expansive comp (b1,b2)) \<in> n_equiv n\<rbrakk> \<Longrightarrow>
+      \<exists>c1 c2. (a=Rep_non_expansive comp (c1,c2) \<and> (c1,b2) \<in> n_equiv n \<and> (c2,b2) \<in> n_equiv n)"
+begin
+definition rep_valid_raw :: "'a \<Rightarrow> sprop" where  "rep_valid_raw = Rep_non_expansive valid_raw"      
+definition rep_core :: "'a \<Rightarrow> 'a option" where  "rep_core = Rep_non_expansive core"
+definition rep_comp :: "('a\<times>'a) \<Rightarrow> 'a" where  "rep_comp = Rep_non_expansive comp"
 
-locale total_camera =
-  camera eq_m valid_raw core comp
-    for eq_m and valid_raw and core and comp +
-  fixes \<epsilon> :: 'a
+definition valid :: "'a \<Rightarrow> bool" where
+  "valid a = (\<forall>n. Rep_sprop (rep_valid_raw a) n)"
+
+definition incl :: "'a \<Rightarrow> 'a \<Rightarrow> bool" where
+  "incl a b = (\<exists>c. b = rep_comp (a,c))"
+
+definition n_incl :: "nat \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" where
+  "n_incl n a b = (\<exists>c. (b, rep_comp (a,c)) \<in> n_equiv n)"
+end
+  
+class total_camera = camera +
+  fixes \<epsilon> :: "'a::ofe"
   assumes \<epsilon>_valid: "valid \<epsilon>"
-    and \<epsilon>_left_id: "comp (\<epsilon>,a) = a"
-    and \<epsilon>_core: "core \<epsilon> = Some \<epsilon>"
+    and \<epsilon>_left_id: "rep_comp (\<epsilon>,a) = a"
+    and \<epsilon>_core: "rep_core \<epsilon> = Some \<epsilon>"
 
 definition (in total_camera) total_core :: "'a \<Rightarrow> 'a" where
-  "total_core a = (case core a of Some a \<Rightarrow> a | None \<Rightarrow> \<epsilon>)"  
+  "total_core a = (case rep_core a of Some a \<Rightarrow> a | None \<Rightarrow> \<epsilon>)"
+
+subsection \<open> Uniform Predicates \<close>
+(* First monotone, non-expansive functions *)  
+typedef (overloaded) 'm mon_ne = "{\<Phi>::('m::camera,sprop) non_expansive. 
+  \<forall>n a b. n_incl n a b \<longrightarrow> n_subseteq n (Rep_non_expansive \<Phi> a) (Rep_non_expansive \<Phi> b)}"
+proof
+  fix s :: sprop
+  define mon_ne where "mon_ne = Abs_non_expansive (\<lambda>_::'m. s)"
+  then have  "\<forall>n a b. n_incl n a b \<longrightarrow> n_subseteq n (Rep_non_expansive mon_ne a) (Rep_non_expansive mon_ne b)"
+    by (simp add: Abs_non_expansive_inverse[of "(\<lambda>_. s)"] ofe_refl n_subseteq_def)
+  thus "mon_ne \<in> {\<Phi>. \<forall>n a b. n_incl n a b \<longrightarrow> n_subseteq n (Rep_non_expansive \<Phi> a) (Rep_non_expansive \<Phi> b)}"
+   by simp  
+qed
+setup_lifting type_definition_mon_ne
+  
+definition rep_mon :: "'a mon_ne \<Rightarrow> 'a::camera \<Rightarrow> sprop" where 
+  "rep_mon = Rep_non_expansive \<circ> Rep_mon_ne"
+
+definition abs_mon :: "('a::camera \<Rightarrow> sprop) \<Rightarrow> 'a mon_ne" where
+  "abs_mon = Abs_mon_ne \<circ> Abs_non_expansive"  
+  
+definition mon_ne_equiv :: "('a::camera) mon_ne \<Rightarrow> 'a mon_ne \<Rightarrow> bool" where
+  "mon_ne_equiv x y = (\<forall>m a. Rep_sprop (rep_valid_raw a) m 
+    \<longrightarrow> (Rep_sprop (rep_mon x a) m \<longleftrightarrow> Rep_sprop (rep_mon y a) m))"
+
+(* Then the actual uniform predicate *)
+quotient_type (overloaded) 'b uprop = "('b::camera) mon_ne" / mon_ne_equiv
+  by (simp add: mon_ne_equiv_def equivp_reflp_symp_transp reflp_def symp_def transp_def)
+
+instantiation uprop :: (camera) cofe begin
+  lift_definition lim_uprop :: "(nat \<Rightarrow> 'a uprop) \<Rightarrow> 'a uprop" is
+    "\<lambda>c. abs_mon (\<lambda>x. Abs_sprop (\<lambda>n. \<forall>n'\<le>n. Rep_sprop (rep_valid_raw x) n' 
+      \<longrightarrow> Rep_sprop (rep_mon (c n') x) n ))"
+  sorry (* This is a direct translation from the Coq formalization, so it should hold. *)
+    
+  lift_definition n_equiv_uprop :: "nat \<Rightarrow> ('a uprop\<times>'a uprop) set" is
+    "\<lambda>n. {(x,y) | x y. \<forall>m\<le>n. \<forall>a. 
+      Rep_sprop (rep_valid_raw a) m \<longrightarrow> (Rep_sprop (rep_mon x a) m \<longleftrightarrow> Rep_sprop (rep_mon y a) m)}" .
+
+  lift_definition ofe_eq_uprop :: "'a uprop \<Rightarrow> 'a uprop \<Rightarrow> bool" is "\<lambda>x y. mon_ne_equiv x y" 
+    unfolding mon_ne_equiv_def by auto
+instance sorry
+end
 end
--- IrisCore/OFEs.thy
+++ IrisCore/OFEs.thy
@@ -8,38 +8,46 @@ text \<open> The definition of OFEs, COFEs and instances of those. \<close>
 subsection \<open> OFE \<close>
 class ofe =
   fixes n_equiv :: "nat \<Rightarrow> ('a\<times>'a) set"
+    and ofe_eq :: "'a \<Rightarrow> 'a \<Rightarrow> bool" (* Allow for custom equality *)
   assumes ofe_refl: "(x,x) \<in> n_equiv n"
     and ofe_sym: "(x,y) \<in> n_equiv n \<longleftrightarrow> (y,x) \<in> n_equiv n"
     and ofe_trans: "\<lbrakk>(x,y) \<in> n_equiv n; (y,z) \<in> n_equiv n\<rbrakk> \<Longrightarrow> (x,z) \<in> n_equiv n"
     and ofe_mono: "m\<le>n \<Longrightarrow> n_equiv n \<subseteq> n_equiv m"
-    and ofe_limit: "(x=y) \<longleftrightarrow> (\<forall>n. (x,y) \<in> n_equiv n)"
+    and ofe_limit: "(ofe_eq x y) \<longleftrightarrow> (\<forall>n. (x,y) \<in> n_equiv n)"
+    
+(* Step indexed propositions. They are defined to hold for all steps below a maximum. *)
+typedef sprop = "{s::nat\<Rightarrow>bool. \<forall>n m. m\<le>n \<longrightarrow> s n \<longrightarrow> s m}"
+proof
+  define s :: "nat\<Rightarrow>bool" where "s = (\<lambda>_. True)"
+  thus "s \<in> {s::nat\<Rightarrow>bool. \<forall>n m. m\<le>n \<longrightarrow> s n \<longrightarrow> s m}" by simp
+qed
+setup_lifting type_definition_sprop
 
-(* 
-  Step indexed propositions.
-  The value is the highest step count for which the proposition holds.
-  A value of nat.Max is equivalent to True propositions, 
-  whereas a value of 0 is equivalent to False.
-*)
-type_synonym sprop = nat
-instantiation nat :: ofe begin
-  definition n_equiv_nat :: "nat \<Rightarrow> (sprop\<times>sprop) set" where
-    "n_equiv_nat n = {(x,y) | x y. \<forall>m\<le>n. m\<le>x \<longleftrightarrow> m\<le>y}"
-instance
-  by (standard, unfold n_equiv_nat_def) (auto, presburger?)
+instantiation sprop :: ofe begin
+  definition n_equiv_sprop :: "nat \<Rightarrow> (sprop\<times>sprop) set" where
+    "n_equiv_sprop n = {(x,y) | x y. \<forall>m\<le>n. Rep_sprop x m \<longleftrightarrow> Rep_sprop y m}"
+  definition ofe_eq_sprop :: "sprop \<Rightarrow> sprop \<Rightarrow> bool" where
+    "ofe_eq_sprop x y = (\<forall>n. (Rep_sprop x n) \<longleftrightarrow> (Rep_sprop y n))"  
+instance by (standard, unfold n_equiv_sprop_def ofe_eq_sprop_def) auto 
 end
+
+lift_definition n_subseteq :: "nat \<Rightarrow> sprop \<Rightarrow> sprop \<Rightarrow> bool" is
+  "\<lambda>n X Y. \<forall>m\<le>n. X m \<longrightarrow> Y m" .
 
 instantiation option :: (ofe) ofe begin
   definition n_equiv_option :: "nat \<Rightarrow> ('a option\<times>'a option) set" where
   "n_equiv_option n = 
     {(x,y) |x y. (\<exists>x' y'. x=Some x'\<and>y=Some y'\<and>(x',y')\<in>n_equiv n) \<or> x=None\<and>y=None}"
-instance proof (standard) 
+  definition ofe_eq_option where 
+    "ofe_eq_option x y \<equiv> (x=None\<and>y=None)\<or>(\<exists>x' y'. x=Some x'\<and>y=Some y'\<and>(ofe_eq x' y'))"
+instance proof (standard)
   fix x y
-  show "((x::'a option) = y) \<longleftrightarrow> (\<forall>n. (x, y) \<in> n_equiv n)"
-  by (auto simp: n_equiv_option_def ofe_refl) (metis ofe_limit option.sel)
+  show "(ofe_eq (x::'a option) y) \<longleftrightarrow> (\<forall>n. (x, y) \<in> n_equiv n)"
+  by (auto simp: n_equiv_option_def ofe_refl ofe_eq_option_def) (metis ofe_limit option.sel)+
 next 
   fix x y n
   show "((x::'a option,y) \<in> n_equiv n) \<longleftrightarrow> ((y,x) \<in> n_equiv n)"
-  apply (auto simp: n_equiv_option_def) using ofe_sym by auto
+    using ofe_sym by (auto simp: n_equiv_option_def)+
 next
   fix m n
   assume assm: "(m::nat)\<le>n"
@@ -50,7 +58,9 @@ end
 
 instantiation prod :: (ofe,ofe) ofe begin
   definition n_equiv_prod :: "nat \<Rightarrow> (('a\<times>'b)\<times>('a\<times>'b)) set" where
-  "n_equiv_prod n = {((x1,y1),(x2,y2)) | x1 x2 y1 y2. (x1,x2) \<in> n_equiv n \<and> (y1,y2) \<in> n_equiv n}"
+    "n_equiv_prod n = {((x1,y1),(x2,y2)) | x1 x2 y1 y2. (x1,x2) \<in> n_equiv n \<and> (y1,y2) \<in> n_equiv n}"
+  fun ofe_eq_prod :: "'a\<times>'b \<Rightarrow> 'a\<times>'b \<Rightarrow> bool" where 
+    "ofe_eq_prod (a1,b1) (a2,b2) = (ofe_eq a1 a2 \<and> ofe_eq b1 b2)"
 instance proof (standard)
   fix x n 
   show "(x::('a\<times>'b),x)\<in>n_equiv n"
@@ -62,23 +72,26 @@ next
   apply (auto simp: n_equiv_prod_def) using ofe_mono assm by auto
 next
   fix x y
-  show "((x::('a\<times>'b))=y) \<longleftrightarrow> (\<forall>n. (x,y)\<in>n_equiv n)"
+  show "(ofe_eq (x::('a\<times>'b)) y) \<longleftrightarrow> (\<forall>n. (x,y)\<in>n_equiv n)"
   apply (auto simp: n_equiv_prod_def)
-  apply (meson surj_pair ofe_limit)
-  by (metis (full_types) ofe_limit prod.inject)
+  apply (meson OFEs.ofe_eq_prod.elims(2) ofe_limit)
+  by (metis (full_types) OFEs.ofe_eq_prod.simps ofe_limit prod.inject)
 qed (auto simp: n_equiv_prod_def ofe_sym intro: ofe_refl ofe_trans)
 end
 
 instantiation unit :: ofe begin
   definition n_equiv_unit :: "nat \<Rightarrow> (unit \<times> unit) set" where
     "n_equiv_unit _ = {((),())}"
+  fun ofe_eq_unit :: "unit \<Rightarrow> unit \<Rightarrow> bool" where "ofe_eq_unit _ _ = True"
 instance by (standard, unfold n_equiv_unit_def) auto
 end
 
 datatype ('a::ofe) later = Next 'a
 instantiation later :: (ofe) ofe begin
   definition n_equiv_later :: "nat \<Rightarrow> ('a later\<times>'a later) set" where
-  "n_equiv_later n = {(Next x, Next y) | x y. n=0 \<or> (x,y) \<in> n_equiv (n-1) }"
+    "n_equiv_later n = {(Next x, Next y) | x y. n=0 \<or> (x,y) \<in> n_equiv (n-1) }"
+  fun ofe_eq_later :: "'a later \<Rightarrow> 'a later \<Rightarrow> bool" where 
+    "ofe_eq_later (Next a) (Next b) = ofe_eq a b"
 instance proof (standard)
   fix x n
   show "(x::'a later,x) \<in> n_equiv n" 
@@ -98,9 +111,10 @@ next
     apply (auto simp: n_equiv_later_def) using ofe_mono[of "m-1" "n-1"] by fastforce
 next
   fix x y :: "'a later"
-  show "(x = y) \<longleftrightarrow> (\<forall>n. (x,y) \<in> n_equiv n)"
+  show "(ofe_eq x y) \<longleftrightarrow> (\<forall>n. (x,y) \<in> n_equiv n)"
     apply (auto simp: n_equiv_later_def intro: later.exhaust ofe_refl)
-    by (metis (mono_tags, hide_lams) ofe_limit diff_Suc_Suc later.inject minus_nat.diff_0 nat.discI)
+    apply (meson ofe_eq_later.elims(2) ofe_limit)
+    by (metis (full_types) OFEs.ofe_eq_later.simps One_nat_def Zero_not_Suc diff_Suc_1 later.inject ofe_limit)
 qed  
 end
 
@@ -108,7 +122,7 @@ subsection \<open> COFE \<close>
 definition is_chain :: "(nat \<Rightarrow> ('t::ofe)) \<Rightarrow> bool" where
   "is_chain c \<equiv> (\<forall>n m. n\<le>m \<longrightarrow> (c m,c n) \<in> n_equiv n)"
 
-locale cofe =
+class cofe = ofe +
   fixes lim :: "(nat \<Rightarrow> ('a::ofe)) \<Rightarrow> 'a"
   assumes core_compl: "is_chain (c::(nat\<Rightarrow>'a)) \<Longrightarrow> (lim c,c n) \<in> n_equiv n"
 end
